d339b0d9b954f8d03a9303c75d2f506d2417a050
Adam Orf <aorf@femmeosage.net>
2024-07-11 11:12:30 -0500

    Update index.js
    working on the inquirer prompts and fs code

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const inquirer = require('inquirer');
-const colors = require('colors');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
 
 inquirer
   .prompt([
@@ -15,12 +17,68 @@ inquirer
     },
     {
       type: 'input',
+      message: 'Project Description',
+      name: 'description',
+    },
+    {
+      type: 'input',
       message: 'What was your motivation?',
       name: 'motivation',
     },
+    {
+      type: 'input',
+      message: 'credits?',
+      name: 'credits',
+    },{
+      type: 'input',
+      message: 'useage instructions?',
+      name: 'instructions',
+    },{
+      type: 'input',
+      message: 'who is it for?',
+      name: 'who',
+    },{
+      type: 'input',
+      message: 'GitHub username',
+      name: 'github',
+    },
+    {
+      type: 'input',
+      message: 'email address',
+      name: 'email',
+    },
+    {
+      type: 'input',
+      message: 'license used?',
+      name:license,
+    },
   ])
-  .then((response) =>
-    response.confirm === response.password
-      ? console.log(colors.green('Success!'))
-      : console.log(colors.red('You forgot your password already?!'))
-  );
+  //generating markdown
+  .then((data) => {
+    let markdown = generateMarkdown ({
+      title: `${data.title}`,
+      description: `${data.description}`,
+      instructions: `${data.instructions}`,
+      who: `${data.who}`,
+      motivation: `${data.motivation}`,
+      name: `${data.name}`,
+      license: `${data.license}`,
+      email: `${data.email}`,
+      github: `${data.github}`,
+    })
+    //writing the file
+    fs.writeFile('README.md', markdown, (err) => {
+        if (err) {
+            console.log(err); 
+        } else {
+            console.log('success');
+        }
+    })
+  });
+
+
+// intializing the app
+function init() {}
+
+// calling the initialization
+init();
